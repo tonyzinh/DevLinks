660cf0d0b27d33ded78b33e5910a32e69317db1a
Tony <entonysantos@outlook.com.br>
2024-12-20 15:09:02 -0300

    final project

---
+++ script.js
@@ -0,0 +1,13 @@
+function ToggleEvent() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Avatar no modo Light")
+  }
+}
